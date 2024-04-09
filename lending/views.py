9378c248d1a1df0f112236140ed38ac02ca40d87
Eremeev Aleksandr <aleksandreremeev75@gmail.com>
2024-04-09 22:46:31 +0300

    fixed bugs

--- lending/views.py
+++ lending/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from telegram import Bot
+def send_notification(text):
+    bot = Bot(token='7128625164:AAG2ZwTPyN8-xkNoIP0wmRNjG-pzIBI1Ncw')
+    chat_id = "543664962"
+    bot.send_message(chat_id=chat_id, text=text)
 
 def home(request):
+    user = request.user
+    send_notification(user,'visit your home page')
     return render(request, 'home.html')
 
 def tsg(request):
