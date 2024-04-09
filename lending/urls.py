78dc6c75b4adc5523f8cd21ab0b4733ec65b07d3
wazowskipuffs <you@example.com>
2024-04-09 14:14:07 +0300

    added tsg page

--- lending/urls.py
+++ lending/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('tsg/', views.tsg, name='tsg'),
 ]
--- lending/views.py
+++ lending/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def tsg(request):
+    return render(request, 'tsg.html')
