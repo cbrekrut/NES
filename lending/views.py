479fd3058ff42353a70d85b18bd417265abe5427
wazowskipuffs <you@example.com>
2024-04-11 00:06:14 +0300

    added modal window

--- lending/views.py
+++ lending/views.py
@@ -6,19 +6,67 @@ TELEGRAM_CHAT_ID = '543664962'
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 def home(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     #bot.send_message(TELEGRAM_CHAT_ID, 'Site is online')
     return render(request, 'home.html')
 
 def tsg(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request, 'tsg.html')
 def md(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request, 'metall-design.html')
 def nanopi(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request, 'nanopi.html')
 def portfolio(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request, 'portfolio.html')
 
 def priliv(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request, 'priliv.html')
 
 def contact(request):
@@ -34,4 +82,12 @@ def contact(request):
     return render(request, 'contact.html')
 
 def about(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        telephone = request.POST.get('telephone')
+        textarea = request.POST.get('Textarea')
+        message = f"Новый заказ!\nИмя: {name}\nEmail: {email}\nТелефон: {telephone}\nОписание: {textarea}"
+        
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     return render(request, 'about.html')
