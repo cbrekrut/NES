da8c63a04bef82c81c305332bf3d34e88fc94ed4
Aleksandr Eremeev <aleksandreremeev75@gmail.com>
2025-06-24 11:04:39 +0300

    server push

--- lending/models.py
+++ lending/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Project(models.Model):
-    preview = models.ImageField(upload_to='media/')
+    preview = models.ImageField(upload_to='')
     name = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     description1 = models.TextField()
-    img1 = models.ImageField(upload_to='media/')
+    img1 = models.ImageField(upload_to='')
     description2 = models.TextField()
-    img2 = models.ImageField(upload_to='media/')
+    img2 = models.ImageField(upload_to='')
     description3 = models.TextField()
-    img3 = models.ImageField(upload_to='media/')
+    img3 = models.ImageField(upload_to='')
     link = models.CharField(max_length=200)
     def __str__(self):
         return self.title
